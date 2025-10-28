facc155e224dca005aa8deffc46678bbd04ab1d9
unknown <aquanataliya@gmail.com>
2025-10-28 18:15:10 +0200

    task-3

--- js/task-3.js
+++ js/task-3.js
@@ -4,10 +4,3 @@ const outputElement = document.getElementById('name-output');
 inputElement.addEventListener('input', () => {
     outputElement.textContent = inputElement.value.trim() || 'Anonymous';
 });
-
-
-// inputElement.addEventListener('input', () => {
-// if(inputElement.value.trim()) {
-//         console.log(outputElement.textContent = inputElement.value.trim())
-//     }else {'Anonymous'};
-// });
